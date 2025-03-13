6dba8d3c4238e64aba4ee5843ca94053748cfc7c
LazuliKao <LazuliKao233@outlook.com>
2025-03-14 00:46:17 +0800

    remove zsh in profile

--- uci-defaults/06_ohmyzsh.sh
+++ uci-defaults/06_ohmyzsh.sh
@@ -25,3 +25,7 @@
 #     exit 1
 # fi
 sed -i "s:/bin/ash:$(which zsh):g" /etc/passwd
+if tail -n 1 /etc/profile | grep -q "zsh"; then
+    head -n -1 /etc/profile >/etc/profile.new
+    mv /etc/profile.new /etc/profile
+fi
