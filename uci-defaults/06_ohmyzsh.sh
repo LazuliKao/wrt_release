1dd7af260e152c43443a362142ae2f5e62a34484
LazuliKao <LazuliKao233@outlook.com>
2025-03-21 10:26:22 +0800

    Update 06_ohmyzsh.sh

--- uci-defaults/06_ohmyzsh.sh
+++ uci-defaults/06_ohmyzsh.sh
@@ -29,3 +29,6 @@ if tail -n 1 /etc/profile | grep -q "zsh"; then
     head -n -1 /etc/profile >/etc/profile.new
     mv /etc/profile.new /etc/profile
 fi
+if [ -f /etc/config/advancedplus ]; then
+    uci set advancedplus.@basic[0].usshmenu=1
+fi
