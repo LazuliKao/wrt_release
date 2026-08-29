3f7eab4243a09f4aedf1a726e5ad0078586c2f04
LazuliKao <LazuliKao233@outlook.com>
2026-08-29 23:46:24 +0800

    feat: add oaf recipe

--- wrt_core/update.sh
+++ wrt_core/update.sh
@@ -115,7 +115,6 @@ stage_pre_install_source_fixes() {
     add_backup_info_to_sysupgrade
     update_mosdns_deconfig
     fix_quickstart
-    update_oaf_deconfig
     add_timecontrol
     add_quickfile
     update_lucky
